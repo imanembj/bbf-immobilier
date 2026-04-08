a0bfa299df2b2b0a7bbca9469e52a77807f672a7
Mandiou Kouate <mandioukouate@Air-de-Mandiou.lan>
2026-04-09 00:43:09 +0200

    Force restart v2

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
 const port = process.env.PORT || 3000
 
+// Force restart - v2
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
